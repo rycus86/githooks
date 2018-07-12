df0b4db18157699e76b922b788eb2a6617390a60
Viktor Adam <Viktor.Adam@ig.com>
2018-07-12 14:58:05 +0100

    Fix exit code on hook failure

--- install.sh
+++ install.sh
@@ -103,34 +103,50 @@ setup_hook_templates() {
 # It allows you to have a .githooks folder per-project that contains
 # its hooks to execute on various Git triggers.
 
+execute_hook() {
+    HOOK_PATH="$1"
+    shift
+
+    RESULT=0
+
+    if [ -x "$HOOK_PATH" ]; then
+        "$HOOK_PATH" "$@"
+        RESULT=$?
+
+    elif [ -f "$HOOK_PATH" ]; then
+        sh "$HOOK_PATH" "$@"
+        
+        RESULT=$?
+    fi
+
+    return $RESULT
+}
+
 HOOK_NAME=$(basename "$0")
 HOOK_FOLDER=$(dirname "$0")
 
 # Execute the old hook if we moved it when installing our hooks.
 if [ -x "${HOOK_FOLDER}/${HOOK_NAME}.replaced.githook" ]; then
 	ABSOLUTE_FOLDER=$(cd "${HOOK_FOLDER}" && pwd)
-    "${ABSOLUTE_FOLDER}/${HOOK_NAME}.replaced.githook" "$@"
+
+    if ! execute_hook "${ABSOLUTE_FOLDER}/${HOOK_NAME}.replaced.githook" "$@"; then
+        exit 1
+    fi
 fi
 
 if [ -d ".githooks/${HOOK_NAME}" ]; then
     # If there is a directory like .githooks/pre-commit,
 	#   then for files like .githooks/pre-commit/lint
     for HOOK_FILE in .githooks/"${HOOK_NAME}"/*; do
-        # Either execute directly or as a Shell script
-        if [ -x "$HOOK_FILE" ]; then
-            "$(pwd)/$HOOK_FILE" "$@"
-        elif [ -f "$HOOK_FILE" ]; then
-            sh "$HOOK_FILE" "$@"
+        if ! execute_hook "$(pwd)/$HOOK_FILE" "$@"; then
+            exit 1
         fi
     done
 
-elif [ -x ".githooks/${HOOK_NAME}" ]; then
-    # Execute the file directly
-    eval "$(pwd)/.githooks/${HOOK_NAME}" "$@"
-
 elif [ -f ".githooks/${HOOK_NAME}" ]; then
-    # Execute as a Shell script
-    sh ".githooks/${HOOK_NAME}" "$@"
+    if ! execute_hook ".githooks/${HOOK_NAME}" "$@"; then
+        exit 1
+    fi
 
 fi
 '
