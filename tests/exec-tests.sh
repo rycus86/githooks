8c22baacd4af0aad90a0ba83e7de37623be9a784
Viktor Adam <rycus86@gmail.com>
2018-08-17 22:11:13 +0100

    Fix the test setup :blue_heart:

--- tests/exec-tests.sh
+++ tests/exec-tests.sh
@@ -12,9 +12,9 @@ RUN git config --global user.email "githook@test.com" && \
 ADD tests/exec-steps.sh tests/step-* /var/lib/tests/
 
 # Change the base template so we can pass in the hook name and accept flags
-RUN sed -i 's|^HOOK_NAME=.*|HOOK_NAME=\${HOOK_NAME:-\$(basename "\$0")}|' /var/lib/githooks/base-template.sh && \\
-    sed -i 's|^HOOK_FOLDER=.*|HOOK_FOLDER=\${HOOK_FOLDER:-\$(dirname "\$0")}|' /var/lib/githooks/base-template.sh && \\
-    sed -i 's|^ACCEPT_CHANGES=.*|ACCEPT_CHANGES=\${ACCEPT_CHANGES}|' /var/lib/githooks/base-template.sh && \\
+RUN sed -i 's|HOOK_NAME=.*|HOOK_NAME=\${HOOK_NAME:-\$(basename "\$0")}|' /var/lib/githooks/base-template.sh && \\
+    sed -i 's|HOOK_FOLDER=.*|HOOK_FOLDER=\${HOOK_FOLDER:-\$(dirname "\$0")}|' /var/lib/githooks/base-template.sh && \\
+    sed -i 's|ACCEPT_CHANGES=.*|ACCEPT_CHANGES=\${ACCEPT_CHANGES}|' /var/lib/githooks/base-template.sh && \\
     sed -i 's|read -r ACCEPT_CHANGES|echo "Accepted: \$ACCEPT_CHANGES" # disabled for tests: read -r ACCEPT_CHANGES|' /var/lib/githooks/base-template.sh
 
 RUN sh /var/lib/tests/exec-steps.sh
