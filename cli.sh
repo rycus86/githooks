bdded803873d925800d0747bc7ecf821ddcc8573
Gabriel Nuetzi <gnuetzi@gmail.com>
2019-08-03 14:55:43 +0200

    Test corrections. :tada:

--- cli.sh
+++ cli.sh
@@ -11,7 +11,7 @@
 # See the documentation in the project README for more information,
 #   or run the `git hooks help` command for available options.
 #
-# Version: 1908.031453-c914f4
+# Version: 1908.031455-bc27e9
 
 #####################################################
 # Prints the command line help for usage and
--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 #   and performs some optional setup for existing repositories.
 #   See the documentation in the project README for more information.
 #
-# Version: 1908.031453-c914f4
+# Version: 1908.031455-bc27e9
 
 # The list of hooks we can manage with this script
 MANAGED_HOOK_NAMES="
@@ -23,7 +23,7 @@ BASE_TEMPLATE_CONTENT='#!/bin/sh
 # It allows you to have a .githooks folder per-project that contains
 # its hooks to execute on various Git triggers.
 #
-# Version: 1908.031453-c914f4
+# Version: 1908.031455-bc27e9
 
 #####################################################
 # Execute the current hook,
@@ -882,7 +882,7 @@ CLI_TOOL_CONTENT='#!/bin/sh
 # See the documentation in the project README for more information,
 #   or run the `git hooks help` command for available options.
 #
-# Version: 1908.031453-c914f4
+# Version: 1908.031455-bc27e9
 
 #####################################################
 # Prints the command line help for usage and
