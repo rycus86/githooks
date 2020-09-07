1bfd3458ca568a29504c4f010eb07c9b7142466e
Gabriel Nützi <gnuetzi@gmail.com>
2020-09-08 01:02:23 +0200

    Coerage bumped to kcov36 :heart:

--- tests/coverage.sh
+++ tests/coverage.sh
@@ -10,11 +10,11 @@ fi
 
 # Build a Docker image on top of kcov with our scripts
 cat <<EOF | docker build --force-rm -t githooks:coverage -f - .
-FROM kcov/kcov:v33
+FROM kcov/kcov:v36
 
 RUN echo 'deb http://deb.debian.org/debian stretch main' >> /etc/apt/sources.list \
     && (apt-get update || true) \
-    && apt-get install -y git
+    && apt-get install -y git python
 
 ADD base-template.sh base-template-wrapper.sh install.sh uninstall.sh cli.sh /var/lib/githooks/
 RUN chmod +x /var/lib/githooks/*.sh
@@ -53,6 +53,20 @@ RUN echo "Make test gitrepo to clone from ..." && \
     git add . && \
     git commit -a -m "Initial release" && \
     git commit -a --allow-empty -m "Empty to reset to trigger update"
+
+RUN useradd -ms /bin/bash coverage
+RUN chown -R coverage:coverage /var /usr/share/git-core
+
+USER coverage
+WORKDIR /home/coverage
+RUN git config --global user.email "githook@test.com" && \
+    git config --global user.name "Githook Tests"
+
+RUN kcov \
+    --coveralls-id="$TRAVIS_JOB_ID" \
+    --include-pattern="/home/coverage/.githooks/release" \
+    ~/cover \
+    "/var/lib/tests/exec-steps.sh"
 EOF
 
 # shellcheck disable=SC2181
@@ -61,17 +75,14 @@ if [ $? -ne 0 ]; then
 fi
 
 # Make sure we delete the previous run results
-docker run --rm --security-opt seccomp=unconfined \
+docker run --user root --rm --security-opt seccomp=unconfined \
     -v "${RUN_DIR}/cover":/cover \
     --entrypoint sh \
     githooks:coverage \
     -c 'rm -rf /cover/*'
 
-# Run the actual tests and collect the coverage info
-docker run --rm --security-opt seccomp=unconfined \
+# Collect the coverage info
+docker run --user root --rm --security-opt seccomp=unconfined \
     -v "${RUN_DIR}/cover":/cover \
     githooks:coverage \
-    --coveralls-id="$TRAVIS_JOB_ID" \
-    --include-pattern="/var/lib/githooks/" \
-    /cover \
-    "/var/lib/tests/exec-steps.sh"
+    bash -c 'cp -r /home/coverage/cover /cover/'
