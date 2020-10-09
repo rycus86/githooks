7039af0089833c01c5dacddde17e04feaaad83ff
Gabriel Nützi <gnuetzi@gmail.com>
2020-10-09 15:21:22 +0200

    Minimalize test :purple_heart:

--- tests/step-120.sh
+++ tests/step-120.sh
@@ -1,22 +1,19 @@
 #!/bin/sh
 # Test:
-#   Custom install prefix test
+#   PR #135: Bugfix: Test that init.templateDir is not set when using core.hooksPath.
 
-TEST_PREFIX_DIR="/tmp/githooks"
+if [ "$(id -u)" != "0" ] || ! echo "$EXTRA_INSTALL_ARGS" | grep -q "use-core-hookspath"; then
+    echo "! Test needs root access and --use-core-hookspath."
+    exit 249
+fi
+
 mkdir -p ~/.githooks/release && cp /var/lib/githooks/cli.sh ~/.githooks/release || exit 1
 
 rm -rf /usr/share/git-core/templates/hooks || exit 1
 
-sh /var/lib/githooks/install.sh --non-interactive --prefix "$TEST_PREFIX_DIR" || exit 1
+sh /var/lib/githooks/install.sh --non-interactive || exit 1
 
-if [ "$(id -u)" != "0" ]; then
-    echo "! Test needs root access."
-    exit 249
-fi
-
-if echo "$EXTRA_INSTALL_ARGS" | grep -q "use-core-hookspath"; then
-    if [ -n "$(git config init.templateDir)" ]; then
-        echo "! Expected to have init.templateDir not set!" >&2
-        exit 1
-    fi
+if [ -n "$(git config init.templateDir)" ]; then
+    echo "! Expected to have init.templateDir not set!" >&2
+    exit 1
 fi
