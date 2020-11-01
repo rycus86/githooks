8cfcea9cb46ac3ae1c061c9deed8399fb18f0188
Gabriel Nützi <gnuetzi@gmail.com>
2020-11-01 22:37:20 +0100

    Expand evn. variables in runner command :purple_heart:

--- githooks/hooks/runner.go
+++ githooks/hooks/runner.go
@@ -2,6 +2,8 @@ package hooks
 
 import (
 	"io/ioutil"
+	"os"
+	"regexp"
 	cm "rycus86/githooks/common"
 
 	"github.com/google/shlex"
@@ -31,7 +33,8 @@ func GetHookRunCmd(hookPath string) ([]string, error) {
 		if e != nil {
 			return nil, cm.ErrorF("Could not read runner file '%s'", runnerFile)
 		}
-		args, e := shlex.Split(string(content))
+
+		args, e := shlex.Split(replaceEnvVariables(string(content)))
 		if e != nil {
 			return nil, cm.ErrorF("Could not parse runner file '%s'", runnerFile)
 		}
@@ -40,4 +43,28 @@ func GetHookRunCmd(hookPath string) ([]string, error) {
 
 	// It does not exists -> default is the shell interpreter.
 	return defaultRunner, nil
+}
+
+var reEnvVariable = regexp.MustCompile(`\$?\$(\{[a-zA-Z]\w*\}|[a-zA-Z]\w*)`)
+
+func replaceEnvVariables(s string) string {
+	return reEnvVariable.ReplaceAllStringFunc(s, substituteEnvVariable)
+}
+
+func substituteEnvVariable(s string) string {
+	r := []rune(s)
+
+	if r[0] == '$' && r[1] == '$' {
+		// Escape '$$var' or '$${var}' => '$var' or '${var}'
+		return string(r[1:])
+	}
+
+	if r[1] == '{' {
+		// Case: '${var}'
+		return os.Getenv(string(r[2 : len(r)-1]))
+	}
+
+	// Case '$var'
+	return os.Getenv(string(r[1:]))
+
 }
