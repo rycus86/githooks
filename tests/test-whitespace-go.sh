e2449cb9a607df65b2b7061abdc5d69dfdb06bf4
Gabriel Nützi <gnuetzi@gmail.com>
2020-12-28 14:15:59 +0100

    Whitespace tests passing :anchor:

---
+++ tests/test-whitespace-go.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+TEST_DIR=$(dirname "$0")
+
+cat <<EOF | docker build --force-rm -t githooks:alpine-lfs-go-whitespace-base -
+FROM golang:1.15.6-alpine
+RUN apk add --no-cache git git-lfs bash
+RUN mkdir -p "/root/whitespace folder"
+ENV HOME="/root/whitespace folder"
+EOF
+
+export ADDITIONAL_INSTALL_STEPS='
+# add a space in paths and wrap in double-quotes
+RUN find /var/lib/tests -name "*.sh" -exec sed -i -E "s|/tmp/test([0-9.]+)|\"/tmp/test \1\"|g" {} \;
+# remove the double-quotes if the path is the only thing on the whole line
+RUN find /var/lib/tests -name "*.sh" -exec sed -i -E "s|^\"/tmp/test([^\"]+)\"|/tmp/test\1|g" {} \;
+'
+
+exec sh "$TEST_DIR"/exec-tests-go.sh 'alpine-lfs-go-whitespace' "$@"
