1bb2e5b1423d6db0e14c5f5524a3384db0b10a6b
Gabriel Nützi <gnuetzi@gmail.com>
2020-12-28 14:41:16 +0100

    Use newest git in tests :anchor:
    - Using old git is another test.

--- tests/test-whitespace-go.sh
+++ tests/test-whitespace-go.sh
@@ -4,7 +4,8 @@ TEST_DIR=$(dirname "$0")
 
 cat <<EOF | docker build --force-rm -t githooks:alpine-lfs-go-whitespace-base -
 FROM golang:1.15.6-alpine
-RUN apk add --no-cache git git-lfs bash
+RUN apk add git git-lfs --update-cache --repository http://dl-3.alpinelinux.org/alpine/edge/main --allow-untrusted
+RUN apk add bash
 RUN mkdir -p "/root/whitespace folder"
 ENV HOME="/root/whitespace folder"
 EOF
