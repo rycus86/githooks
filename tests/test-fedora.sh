369570b37da994daa58746932e27370a1eac28a8
Viktor Adam <rycus86@gmail.com>
2025-05-20 14:41:17 +1000

    Add awk in the Fedora test image :sparkles:

--- tests/test-fedora.sh
+++ tests/test-fedora.sh
@@ -4,7 +4,7 @@ TEST_DIR=$(dirname "$0")
 
 cat <<EOF | docker build --force-rm -t githooks:fedora-base -
 FROM fedora
-RUN dnf install -y git findutils
+RUN dnf install -y git awk findutils
 EOF
 
 exec sh "$TEST_DIR"/exec-tests.sh 'fedora' "$@"
