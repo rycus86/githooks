c76a661f530cfd8b9939123bccc604336498cd0d
Viktor Adam <rycus86@gmail.com>
2025-08-26 12:04:43 +1000

    Fix tests on debian-user :tada:

--- tests/test-debian-user.sh
+++ tests/test-debian-user.sh
@@ -6,7 +6,7 @@ cat <<EOF | docker build --force-rm -t githooks:debian-user-base -
 FROM debian
 RUN apt-get update && \
     apt-get install --no-install-recommends --no-install-suggests -y \
-        git-core ca-certificates && \
+        git-core ca-certificates adduser && \
     adduser --disabled-password -u 1099 test && \
     mkdir -p /var/lib/githooks /var/backup/githooks /usr/share/git-core/templates/hooks && \
     chown -R test:test /var/lib/githooks /var/backup/githooks /usr/share/git-core
